07645aa928bb0bf31f48002a81916ab771e241dc
MoGa2 <Tiennenet@gmail.com>
2019-02-14 03:58:03 +0100

    Start cleaning

--- synchroniser.js
+++ synchroniser.js
@@ -1,116 +1,172 @@
-define([], function(){
-	var Synchroniser = function ( asuccess_cbs, ais_ordered, launch_init_at_start ) 
+/*
+define([""], function(){*/
+	/**
+	 * Use semaphore to synchronize severals ASYNC.
+	 * @exports Synchroniser
+	 * @param {fn[]} callbacks - Callbacks called when all locks are released.
+	 * @param {Boolean} isOrdered - Does lock release needs to be done in order.
+	 * @param {Boolean} launch_init_at_start - Short hand to finalize the init state.
+	 */
+	var Synchroniser = function ( acallbacks, aisOrdered, launch_init_at_start ) 
 	{
-		ais_ordered = ais_ordered || false;
-		var closure_this =  this;
 		var unique_num_lock = 0;
+		var startReleaser;
+		var initReleased;
+		/** True when all locks are released */
 		var open = false;
+
+		/** Callbacks called when all locks are released */
+		var callbacks = acallbacks;
+
+		// ORDER REALTED VARIABLES------------------------
+		var isOrdered = aisOrdered || false;
+		var currentLockOrder = 0;
+		var maxOrder = 0;
+		var quededWaitingLocks = {};
+				
+		/** Object Literal whose propertys are { {string} lock_id : {boolean} is_open } 
+		( PUBLIC FOR DEBUG PURPOSE ) */
 		this.locks = {};
-		this.success_cbs = asuccess_cbs;
-			//	this.on_lock_released = aon_lock_released;
-				this.is_ordered = ais_ordered;
-				var current_lock_order = 0;
-				var max_order = 0;
-				var queded_waiting_locks = {};
-				
-				this.Execute_next_lock_if_already_released = function()
-				{
-					current_lock_order++
-					if ( Object.keys( queded_waiting_locks ).indexOf( current_lock_order ) > -1 )
-					{
-						var on_lock_released = queded_waiting_locks[ current_lock_order ];
-						on_lock_released();
-						this.Execute_next_lock_if_already_released()
-					}
-				}
-				
-				this.Add_one_lock = function( lock )
-				{
-					lock = this.get_unique_lock( lock );
-					//lock.on_lock_released = aon_lock_released;
-					lock.order = max_order++;
+		/**
+		 * Add one lock to the Synchroniser
+		 * @param {string} lock - Lock's id ( DEBUG PURPOSE ).
+		 * @return {fn} releaser - Function who can release the lock.
+		 */
+
+		this.addLock = function( lock )
+		{
+			lock = internal.getUniqueLock( lock );
+			lock.order = maxOrder++;
 					
-					this.locks[ lock ] = true;
+			this.locks[ lock ] = true;
 				
-					//this.order = max_order;
-					var releaser = function( on_lock_released ) 
-					{	
-					//	lock.on_lock_released = on_lock_released;
-						if ( !closure_this.is_ordered || closure_this.current_lock_order == lock.order )
-						{
-							on_lock_released && typeof ( on_lock_released ) == "function" && on_lock_released();
-							delete closure_this.locks[ lock ];
-							if ( isEmpty( closure_this.locks ) )
-							{
-								closure_this.on_All_lock_opened();
-							}
+			var releaser = ( on_lock_released ) => 
+			{	
+				if ( !isOrdered || currentLockOrder == lock.order )
+				{
+					on_lock_released && typeof ( on_lock_released ) == "function" && on_lock_released();
+					delete this.locks[ lock ];
+					if ( isEmpty( this.locks ) )
+					{
+						internal.onAllLockReleased();
+					}
 							
-							closure_this.Execute_next_lock_if_already_released();
-						}
-						else{
-							queded_waiting_locks[ lock.order ] = lock;
-						}
-					}
-					releaser.id = lock;
-					return releaser;
-				};
-				 
-		
-				this.get_unique_num_lock = function()
-				{
-					return unique_num_lock++;
+					internal.executeNextLockIfAlreadyReleased();
 				}
-		
-				this.get_unique_lock = function( lock )
+				else
 				{
-					var unique_num_lock = this.get_unique_num_lock();
-					return lock + "_" + unique_num_lock;
+					quededWaitingLocks[ lock.order ] = lock;
 				}
+			}
+			releaser.id = lock;
+			return releaser;
+		};
 		
-				this.on_All_lock_opened = function () 
-				{
-					for ( var i = 0; i < this.success_cbs.length ; i++ )
-					{
-						var current_cb = this.success_cbs[ i ];
-						current_cb && current_cb();
-					}
+		/**
+		 * Add a callback without adding a lock or execute the callback if all locks have already been released
+		 * @param {fn} cb - callback.
+		 */
+
+		this.addCallback = function ( cb ) 
+		{
+			if ( open )
+				cb();
+			else
+				callbacks.push( cb );
+		};
+
+		/**
+		 * Release the start lock by calling startReleaser 
+		 * Called by the user or on init
+		 */
+
+		this.InitOver  = function()
+		{
+			if ( initReleased )
+				return;
 					
-					open = true;
-				};
-			
-				this.Add_callback = function ( cb ) 
-				{
-					if ( open )
-						cb();
-					else
-						asuccess_cbs.push( cb );
-				};
-				
-				
-				this.InitOver  = function()
-				{
-					if ( this.init_released )
-						return;
-					
-					this.start_releaser();
-					this.init_released = true;
-				};
-				
-				function isEmpty(map) 
-				{
-					for(var key in map) 
-					{
-						if (map.hasOwnProperty(key)) 
-						{
-							return false;
-						}
-					}
-					return true;
-				}
-				
-				this.start_releaser = this.Add_one_lock( "start" );
+			startReleaser();
+			initReleased = true;
+		};
+
+		// KEEP BINDING IN NESTED CLOSURE ( releaser )
+		var internal = 
+		{
+			init : function()
+			{
+				startReleaser = this.addLock( "START" );
 				launch_init_at_start && this.InitOver();
+			},
+			/**
+			 * Recursive fn that check if locks are waiting to be released ( quededWaitingLocks)
+			 * Called when a lock is released.
+			 * 
+			 */
+			executeNextLockIfAlreadyReleased : () =>
+			{
+				currentLockOrder++
+				if ( Object.keys( quededWaitingLocks ).indexOf( currentLockOrder ) > -1 )
+				{
+					var on_lock_released = quededWaitingLocks[ currentLockOrder ];
+					on_lock_released();
+					executeNextLockIfAlreadyReleased()
+				}
+			},
 				
+			/**
+			 * Recursive fn that check if locks are waiting to be released ( quededWaitingLocks)
+			 * Called when a lock is released.
+			 *( SHOULD BE PRIVATE ) 
+			 */
+			getUniqueNumLock : () =>
+			{
+				return unique_num_lock++;
+			},
+			
+			/**
+			 * Create a "unique" id .
+			 * Called when a lock is added.
+			 * ( SHOULD USE SYMBOL ) ( SHOULD BE PRIVATE ) 
+			 */
+			getUniqueLock : ( lock ) =>
+			{
+				var unique_num_lock = internal.getUniqueNumLock();
+				return lock + "_" + unique_num_lock;
+			},
+			
+			/**
+			 * Execute all callbacks and change the state to open
+			 * Called when all locks are open.
+			 * ( SHOULD USE SYMBOL )
+			 */
+			onAllLockReleased : () => 
+			{
+				for ( var i = 0; i < callbacks.length ; i++ )
+				{
+					var current_cb = callbacks[ i ];
+					current_cb && current_cb();
+				}
+						
+				open = true;
+			}		
+		}	
+
+				
+		function isEmpty(map) 
+		{
+			for(var key in map) 
+			{
+				if (map.hasOwnProperty(key)) 
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+		
+		internal.init.apply(this);
+
 	};
-	return Synchroniser;
-});
+	//return Synchroniser;
+//}
+//);
